9066dcf83b9ec61680b97aae12f9b6f7111c0ae3
Inesa Kostova <kostovainesa@gmail.com>
2025-02-26 14:31:49 +0200

    Add files via upload

---
+++ Clever Lilly.cs
@@ -0,0 +1,27 @@
+﻿int age = int.Parse(Console.ReadLine());
+double price = double.Parse(Console.ReadLine());
+int toyPrice = int.Parse(Console.ReadLine());
+double sum =0;
+int money = 10;
+
+for(int i = 1; i<=age; i++)
+{if(age%2==0)
+    {
+        sum += 10 - 1;
+        money += 10;
+    }
+    else
+    {
+        sum += toyPrice;
+    }
+
+}
+
+if(sum>=price)
+{
+    Console.WriteLine($"Yes! {sum-price:F2}");
+}
+else
+{
+    Console.WriteLine($"No! {price-sum:F2}");
+}
