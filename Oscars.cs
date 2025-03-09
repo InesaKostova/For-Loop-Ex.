8f027983515eb2261da736cf243134d79ad905b5
Inesa Kostova <kostovainesa@gmail.com>
2025-03-09 08:48:40 +0200

    Oscars.cs

--- Oscars.cs
+++ Oscars.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = Console.ReadLine();
+double points = double.Parse(Console.ReadLine());
+int count=int.Parse(Console.ReadLine());
+double givenPoints = points;
+for(int i=1; i<=count; i++)
+{
+    string nameJury = Console.ReadLine();
+    double pointsJury = double.Parse(Console.ReadLine());
+    givenPoints += (nameJury.Length * pointsJury / 2);
+    if(givenPoints>=1250.5)
+    {
+        Console.WriteLine($"Congratulations, {name} got a nominee for leading role with {givenPoints:F1}!");
+        break;
+    }
+}
+if(givenPoints<1250.5)
+{
+    Console.WriteLine($"Sorry, {name} you need {1250.5-givenPoints:F1} more!");
+}
